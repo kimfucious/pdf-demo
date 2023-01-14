7c6633c36ba248b592ec4faa2dad91296c294fa6
Kimfucious <kim@kimford.me>
2023-01-14 22:53:36 +0700

    implement height offsets

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,26 +1,43 @@
 import { AppRoute } from "../types/routes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/home";
-import NotFound from "../pages/notFound";
-import SignIn from "../pages/signIn";
-import NavBar from "../components/NavBar";
-import Pdf from "../pages/pdf";
+import { ElHeight, ElName } from "../types";
 import { useAppSelector } from "../hooks/reduxHooks";
+import { useMemo, useState } from "react";
+import Home from "../pages/home";
+import NavBar from "../components/NavBar";
+import NotFound from "../pages/notFound";
+import Pdf from "../pages/pdf";
 import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
+import SignIn from "../pages/signIn";
 
 export default function AppRouter() {
     const { username } = useAppSelector((state) => state.auth);
+    const [heights, setHeights] = useState<ElHeight[]>([]);
+    const { totalOffset, navbarOffset } = useMemo(() => {
+        let hOffset = 0;
+        let nOffset = 56;
+        if (heights.length) {
+            hOffset = heights.reduce((acc, curr) => acc + curr.height, 0);
+            nOffset =
+                heights.find((item) => item.name === ElName.NAVBAR)?.height ??
+                56;
+        }
+        return { totalOffset: hOffset, navbarOffset: nOffset };
+    }, [heights]);
     return (
         <BrowserRouter>
-            <NavBar />
+            <NavBar heights={heights} setHeights={setHeights} />
             <Routes>
-                <Route path={AppRoute.ROOT} element={<Home />} />
+                <Route
+                    path={AppRoute.ROOT}
+                    element={<Home navbarOffset={navbarOffset} />}
+                />
                 <Route
                     path={AppRoute.AUTH}
                     element={
                         <PublicRoute username={username}>
-                            <SignIn />
+                            <SignIn navbarOffset={navbarOffset} />
                         </PublicRoute>
                     }
                 />
@@ -28,7 +45,12 @@ export default function AppRouter() {
                     path={AppRoute.PDF}
                     element={
                         <PrivateRoute username={username}>
-                            <Pdf />
+                            <Pdf
+                                heights={heights}
+                                navbarOffset={navbarOffset}
+                                setHeights={setHeights}
+                                totalOffset={totalOffset}
+                            />
                         </PrivateRoute>
                     }
                 />
