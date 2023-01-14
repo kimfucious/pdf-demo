11130af55a7ef840874b17bb8c50e4565d70bc54
Kimfucious <kim@kimford.me>
2023-01-14 22:51:39 +0700

    remove margin; change title

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,15 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import { AppRoute } from "../../types";
-import { navHeight } from "../../constants";
 import felix from "../../assets/images/felix-pub.gif";
+
 export default function NotFound() {
     const navigate = useNavigate();
     return (
         <div
             className="container py-5 d-flex flex-column align-items-center justify-content-center"
-            style={{ height: "100vh", marginTop: navHeight}}
+            style={{ height: "100vh"}}
         >
-            <h1 className="display-4">Page Not Found</h1>
+            <h1 className="fw-light text-muted">There's nothing here!</h1>
             <img
                 src={felix}
                 alt="Felix at the Pub"
