869499f08a6605d98fe3caf86bf502f5d53e6950
Kimfucious <kim@kimford.me>
2023-01-17 23:30:15 +0700

    remove dead code

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,30 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
-// import { composeWithDevTools } from 'redux-devtools-extension'
 import { AnyAction } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { ThunkAction } from "redux-thunk";
+import auth from "../reducers/authReducer";
 import logger from "redux-logger";
-import auth from "../reducers/authReducer"
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const composeEnhancers =
-//   (window["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] as typeof compose) || compose;
 
 const store = configureStore({
-  reducer: {
-    auth,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }).concat(logger),
+    reducer: {
+        auth,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({ serializableCheck: false }).concat(logger),
 });
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  AnyAction
->
-
-// export type RootState = ReturnType<typeof store.getState>
-// export type AppDispatch = typeof store.dispatch
+    ReturnType,
+    RootState,
+    unknown,
+    AnyAction
+>;
