c6f343fcffc2377a92d0306a6781e6e154a91132
Kimfucious <kim@kimford.me>
2023-01-17 23:02:52 +0700

    remove i tag

--- src/components/NavBar/components/DarkModeToggleButton.tsx
+++ src/components/NavBar/components/DarkModeToggleButton.tsx
@@ -4,14 +4,16 @@ import { MoonStars, Sun } from "tabler-icons-react";
 export default function DarkModeToggleButton() {
     const { theme, toggleTheme } = useColorTheme();
     return (
-        <button className="btn" onClick={() => toggleTheme()}>
-            <i>
-                {theme === "dark" ? (
-                    <Sun color="var(--bs-warning-text)" />
-                ) : (
-                    <MoonStars color="var(--bs-info-text)" />
-                )}
-            </i>
+        <button
+            className="btn btn-link pe-0"
+            onClick={() => toggleTheme()}
+            style={{ textDecoration: "none" }}
+        >
+            {theme === "dark" ? (
+                <Sun color="var(--bs-warning-text)" />
+            ) : (
+                <MoonStars color="var(--bs-info-text)" />
+            )}
         </button>
     );
 }
